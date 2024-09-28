3a8be7fd138f599d452251beffd8fe350a340f8f
BurningProcessor <shmatkoshaman@gmail.com>
2024-09-28 19:21:52 +0300

    add: sort for events add: search for members fix: layout

--- src/pages/Dasboard.tsx
+++ src/pages/Dasboard.tsx
@@ -3,12 +3,15 @@ import { instance } from '../api/axios.api'
 import { IDoing } from '../types/types'
 import { NavLink } from 'react-router-dom'
 import { formatDate } from '../helpers/date.hepler'
+import { SlArrowDown, SlArrowUp } from 'react-icons/sl'
 
 const Dashboard: FC = () => {
 	const [doings, setDoings] = useState<IDoing[]>([])
 
-	async function getDoings() {
-		const response = await instance.get('doing')
+	async function getDoings(sort?: string) {
+		let uri = 'doing'
+		if (sort) uri = uri + '?sort=' + sort
+		const response = await instance.get(uri)
 		setDoings(response.data)
 	}
 
@@ -19,21 +22,56 @@ const Dashboard: FC = () => {
 
 	return (
 		<>
-			<h1>Events</h1>
-			<div className="flex gap-5">
+			<div className="flex justify-between px-2">
+				<h1>Events</h1>
+				<div className="flex gap-2">
+					<fieldset id="sortset" className="flex flex-wrap gap-2">
+						<legend>Order:</legend>
+						<div>
+							<button className="btn btn-blue mr-2" onClick={() => getDoings('titleDown')}>
+								Title <SlArrowDown className="inline" />
+							</button>
+							<button className="btn btn-blue" onClick={() => getDoings('titleUp')}>
+								Title <SlArrowUp className="inline" />
+							</button>
+						</div>
+						<div>
+							<button className="btn btn-blue mr-2" onClick={() => getDoings('dateDown')}>
+								Date <SlArrowDown className="inline" />
+							</button>
+							<button className="btn btn-blue" onClick={() => getDoings('dateUp')}>
+								Date <SlArrowUp className="inline" />
+							</button>
+						</div>
+						<div>
+							<button className="btn btn-blue mr-2" onClick={() => getDoings('organizerDown')}>
+								Organizer <SlArrowDown className="inline" />
+							</button>
+							<button className="btn btn-blue" onClick={() => getDoings('organizerUp')}>
+								Organizer <SlArrowUp className="inline" />
+							</button>
+						</div>
+					</fieldset>
+				</div>
+			</div>
+			<div className="flex flex-wrap gap-2 px-2 py-2">
 				{doings?.map((doing, idx) => (
-					<div className="card rounded-md p-2" key={idx}>
-						<h1>{doing.title}</h1>
-						<p className="mb-3">{doing.desc}</p>
-						<p>{formatDate(doing.date)}</p>
-						<p>{doing.organizer}</p>
-						<div className="my-2">
-							<NavLink to={`doing/${doing.id}/members`} className="btn btn-blue m-1">
-								Members
-							</NavLink>
-							<NavLink to={`doing/${doing.id}/registrate`} className="btn btn-green m-1">
-								Registrate
-							</NavLink>
+					<div className="card flex flex-col justify-between gap-3 rounded-md p-2" key={idx}>
+						<div>
+							<h2>{doing.title}</h2>
+							<p>{doing.desc}</p>
+						</div>
+						<div>
+							<p>{formatDate(doing.date)}</p>
+							<p>{doing.organizer}</p>
+							<div className="my-2">
+								<NavLink to={`doing/${doing.id}/members`} className="btn btn-blue m-1">
+									Members
+								</NavLink>
+								<NavLink to={`doing/${doing.id}/registrate`} className="btn btn-green m-1">
+									Registrate
+								</NavLink>
+							</div>
 						</div>
 					</div>
 				))}
--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -1,35 +1,80 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { instance } from '../api/axios.api'
-import { IDoing, IRegistrationData } from '../types/types'
+import { IDoing, IRegistration, IRegistrationData } from '../types/types'
 import { NavLink, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { AxiosResponse } from 'axios'
 
 const Members: FC = () => {
 	const { doingId } = useParams()
+
 	const [doing, setDoing] = useState<IDoing>()
+	const [members, setMembers] = useState<IRegistration[]>([])
+
+	const fieldNameRef = useRef<HTMLInputElement>(null)
+	const fieldEmailRef = useRef<HTMLInputElement>(null)
 
 	async function getDoing() {
-		const response = await instance.get(`doing/${doingId}`)
+		const response: AxiosResponse<IDoing> = await instance.get(`doing/${doingId}`)
 		setDoing(response.data)
+	}
+	async function getMembers(addData?: string) {
+		let requestData = `registration?doing=${doingId}`
+		if (addData) requestData = requestData + addData
+		const response: AxiosResponse<IRegistration[]> = await instance.get(requestData)
+		setMembers(response.data)
+	}
+
+	function getSearch() {
+		const nameValue = fieldNameRef.current?.value
+		const emailValue = fieldEmailRef.current?.value
+
+		let requestData = '&'
+		if (nameValue) requestData = requestData + `name=${nameValue}`
+		if (nameValue && emailValue) requestData = requestData + '&'
+		if (emailValue) requestData = requestData + `email=${emailValue}`
+
+		if (requestData != '&') setReq(requestData)
+		if (requestData != '&') getMembers(requestData)
+		else getMembers()
 	}
 
 	useEffect(() => {
 		getDoing()
+		getMembers()
 	}, [])
 
 	return (
 		<>
-			<h1>Participants of {doing?.title}</h1>
-			<div className="my-2 flex">
-				<NavLink to={'/'} className="btn btn-blue ">
-					Back to Dashboard
-				</NavLink>
+			<h1 className="m-2">Participants of {doing?.title}</h1>
+			<div className="m-2 flex items-center justify-between gap-2">
+				<div>
+					<NavLink to={'/'} className="btn btn-blue text-nowrap">
+						Back to Dashboard
+					</NavLink>
+				</div>
+				{members.length != 0 ? (
+					<fieldset id="member-search" className="flex flex-wrap justify-start gap-2 ">
+						<legend>Search</legend>
+						<label htmlFor="name">
+							<span>Name</span>
+							<input ref={fieldNameRef} type="search" name="name" />
+						</label>
+						<label htmlFor="email">
+							<span>Email</span>
+							<input ref={fieldEmailRef} type="email" name="email" />
+						</label>
+						<button className="btn btn-green" onClick={() => getSearch()}>
+							Search
+						</button>
+					</fieldset>
+				) : null}
 			</div>
-			<div className="flex gap-5">
-				{doing?.registers?.length != 0 ? (
-					doing?.registers?.map((member, idx) => (
+			<div className="m-2 flex flex-wrap gap-2">
+				{members.length != 0 ? (
+					members.map((member, idx) => (
 						<div className="card rounded-md" key={idx}>
-							<h1>{member.name}</h1>
+							<h2>{member.name}</h2>
 							<p>{member.email}</p>
 						</div>
 					))
